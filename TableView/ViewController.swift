2a85c3d445fdf6e4be7fa7d904c7af74a0532e4b
Emily Coggins <139669664+embcogg@users.noreply.github.com>
2024-02-24 21:02:07 -0600

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Emily Coggins on 2/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
